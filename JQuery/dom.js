5b451c06a8c465166c050a3fffba3f8321c13910
jackieh205 <jacquelineharper90@gmail.com>
2020-09-24 12:36:53 -0500

    second commit

--- JQuery/dom.js
+++ JQuery/dom.js
@@ -1,8 +1,46 @@
+$(document).ready(function () {
+    let btn = $('<input type="button" value="new button" />');
+    $("body").append(btn);
 
-let btn = $('<input type="button" value="new button" />');
-$("body").append(btn);
+    btn.click(function () {
+        alert("Button clicked");
 
-btn.click(function(){
-    alert("Button clicked");
+    });
+
+
+
+
+    $("#div1").mouseover(function () {
+        $('#div1').css({
+            'backgroundColor': 'blue',
+
+
+        })
+
+            .mouseleave(function () {
+                $('#div1').css({
+                    'backgroundColor': '',
+                })
+            })
+
+        $("#myBtn").click(function () {
+            var str = $("#myInput").val();
+            alert(str);
+
+        })
+
+
+
+        var input = $("form input:text").css({
+            background: "white",
+            border: "1px black solid"
+
+
+
+        });
+
+
+
+    });
 
 })
